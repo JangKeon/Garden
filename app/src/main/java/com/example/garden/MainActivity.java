5b221170de822d92b3f8790e552e73293a27b585
SUPERKEON\keon <up2sky7@naver.com>
2021-10-31 18:29:38 +0900

    Log In / Sign Up Complete

--- app/src/main/java/com/example/garden/MainActivity.java
+++ app/src/main/java/com/example/garden/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.garden;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +18,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        // 로그인 된 상태가 아니라면
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startMyActivity(LoginActivity.class);
+        } else {
+
+        }
+    }
+
+    private void startMyActivity(Class c) {
+        Intent intent = new Intent(this, c);
+        startActivity(intent);
     }
 }
+
