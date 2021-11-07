624f68068eafe7350596cd25969883f141d9484a
Euna Ahn <eunaahn2001@icloud.com>
2021-11-07 13:18:03 +0900

    master merge

--- app/src/main/java/com/example/garden/FeedFragment.java
+++ app/src/main/java/com/example/garden/FeedFragment.java
@@ -9,14 +9,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class FeedFragment extends Fragment {
+    @Nullable
+    private FirebaseFirestore firestore;
     private View view;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.feed_frag,container, false);
-        
+        // Inflate the layout for this fragment
+
         return view;
     }
+
+
 }
