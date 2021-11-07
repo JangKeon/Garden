c5b35f69a08be7d39537d5c1b34d2cf35ac55e3c
SeoyoonPark <tjdbs118@gmail.com>
2021-11-07 11:32:17 +0900

    서윤 수정

--- app/src/main/java/com/example/garden/HomeFragment.java
+++ app/src/main/java/com/example/garden/HomeFragment.java
@@ -1,16 +1,24 @@
 package com.example.garden;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
     private View view;
+    private ImageButton btnLivingRoom;
+    private ImageButton post1;
 
     @Nullable
     @Override
@@ -18,5 +26,25 @@ public class HomeFragment extends Fragment {
         view=inflater.inflate(R.layout.home_frag,container, false);
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //setHasOptionsMenu(true);
+
+        btnLivingRoom = view.findViewById(R.id.btnLivingRoom);
+        post1 = view.findViewById(R.id.post1);
+
+        btnLivingRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), LivingRoomActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
