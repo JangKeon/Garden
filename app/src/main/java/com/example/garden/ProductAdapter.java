9c4113e0df04a2b9b0cf9441e0ce1e5c42fb9634
hayoon <gkdbs09@naver.com>
2021-11-15 22:38:20 +0900

    product page

---
+++ app/src/main/java/com/example/garden/ProductAdapter.java
@@ -0,0 +1,25 @@
+package com.example.garden;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class ProductAdapter extends FragmentStatePagerAdapter {
+
+
+    public ProductAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
