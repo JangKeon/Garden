57c303a7a442b828be8dc74b518ab8063d7aefe3
hayoon <gkdbs09@naver.com>
2021-11-17 03:18:07 +0900

    store page

--- app/src/main/java/com/example/garden/ProductAdapter.java
+++ app/src/main/java/com/example/garden/ProductAdapter.java
@@ -7,19 +7,37 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 
 public class ProductAdapter extends FragmentStatePagerAdapter {
 
+    private int tabCount;
 
-    public ProductAdapter(@NonNull FragmentManager fm, int behavior) {
-        super(fm, behavior);
+    public ProductAdapter(@NonNull FragmentManager fm, int tabCount) {
+        super(fm);
+        this.tabCount = tabCount;
     }
 
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        return null;
+
+        switch(position){
+            case 0:
+                Product1_info frag1 = new Product1_info();
+                return frag1;
+            case 1:
+                Product1_review frag2 = new Product1_review();
+                return frag2;
+            case 2:
+                Product1_inquiry frag3 = new Product1_inquiry();
+                return frag3;
+
+            default:
+                return null;
+
+        }
+
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return tabCount;
     }
 }
---
+++ app/src/main/java/com/example/garden/ShoppingCartActivity.java
@@ -0,0 +1,14 @@
+package com.example.garden;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class ShoppingCartActivity extends Activity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
+    }
+
+}
