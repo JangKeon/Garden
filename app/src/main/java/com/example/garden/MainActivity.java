af63169f880006ab9a41b85d447ea4c36d1c7e62
Euna Ahn <eunaahn2001@icloud.com>
2021-11-24 15:21:32 +0900

    MainActivity

--- app/src/main/java/com/example/garden/MainActivity.java
+++ app/src/main/java/com/example/garden/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.garden;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -29,7 +36,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-public class MainActivity extends AppCompatActivity {
+import net.daum.android.map.MapView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private BottomNavigationView bottomNavigationView;
     private FragmentManager fm;
@@ -37,17 +49,70 @@ public class MainActivity extends AppCompatActivity {
     private HomeFragment homeFragment;
     private StoreFragment storeFragment;
     private FeedFragment feedFragment;
-    private GPSFragment gpsFragment;
+    private FragmentUploaded fragmentUploaded;
+    //private GPSFragment gpsFragment;
     private MyPageFragment myPageFragment;
-
+    private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
     private Context context = this;
+    private View navheader;
+    private FrameLayout contentFL;
+    //private MapView mapView;
+    Boolean isUploaded = false;
+
+//    private void getHashKey(){
+//        PackageInfo packageInfo = null;
+//        try {
+//            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+//        } catch (PackageManager.NameNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        if (packageInfo == null)
+//            Log.e("KeyHash", "KeyHash:null");
+//
+//        for (Signature signature : packageInfo.signatures) {
+//            try {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                String keyHash = Base64.encodeToString(md.digest(), Base64.DEFAULT);
+//                Log.d("KeyHash", keyHash);
+//
+//            } catch (NoSuchAlgorithmException e) {
+//                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+//            }
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        isUploaded = intent.getBooleanExtra("isUploaded", false);
 
+        //getHashKey();
+
+        // Toolbar 셋팅
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false); // 기존 title 지우기
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 만들기
+        actionBar.setHomeAsUpIndicator(R.drawable.drawer_menu); //뒤로가기 버튼 이미지 지정
+
+        // DrawerLayout 셋팅
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        //mapView = new net.daum.mf.map.api.MapView(this);
+        //ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        //mapViewContainer.addView(mapView);
+
+        // 메뉴틀릭시 이벤트 셋팅
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         bottomNavigationView = findViewById(R.id.bottomNavi);
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -75,41 +140,17 @@ public class MainActivity extends AppCompatActivity {
         homeFragment = new HomeFragment();
         storeFragment = new StoreFragment();
         feedFragment = new FeedFragment();
-        gpsFragment = new GPSFragment();
+        //gpsFragment = new GPSFragment();
         myPageFragment = new MyPageFragment();
-        setFrag(0);
+        fragmentUploaded = new FragmentUploaded();
 
-        // Drawer 구현
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayShowTitleEnabled(false); // 기존 title 지우기
-        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 만들기
-        actionBar.setHomeAsUpIndicator(R.drawable.drawer_menu); //뒤로가기 버튼 이미지 지정
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-                menuItem.setChecked(true);
-                mDrawerLayout.closeDrawers();
-
-                int id = menuItem.getItemId();
-                String title = menuItem.getTitle().toString();
-
-                if(id == R.id.history){
-                    Toast.makeText(context, title + ": 계정 정보를 확인합니다.", Toast.LENGTH_SHORT).show();
-                }
-                else if(id == R.id.logout){
-                    Toast.makeText(context, title + " Success!", Toast.LENGTH_SHORT).show();
-                    signOut();
-                    startMyActivity(LoginActivity.class);
-                }
-                return true;
-            }
-        });
-        // Drawer 구현
+        if (isUploaded == true) {
+            setFrag(2);
+            bottomNavigationView.setSelectedItemId(R.id.action_feed);
+        }
+        else {
+            setFrag(0);
+        }
 
 
         // 로그인 된 상태가 아니라면
@@ -131,7 +172,14 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
 
     private void signOut() {
         FirebaseAuth.getInstance().signOut();
@@ -180,11 +228,20 @@ public class MainActivity extends AppCompatActivity {
                 ft.commit();
                 break;
             case 2:
-                ft.replace(R.id.main_frame, feedFragment);
+                if (isUploaded == true) {
+                    ft.replace(R.id.main_frame, fragmentUploaded); // 업로드된 프레그먼
+                }
+                else {
+                    ft.replace(R.id.main_frame, feedFragment);
+                }
                 ft.commit();
                 break;
             case 3:
-                ft.replace(R.id.main_frame, gpsFragment);
+                //ft.replace(R.id.main_frame, gpsFragment);
+                ft.remove(homeFragment);
+                ft.remove(storeFragment);
+                ft.remove(feedFragment);
+                ft.remove(myPageFragment);
                 ft.commit();
                 break;
             case 4:
@@ -193,5 +250,24 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        menuItem.setChecked(true);
+        mDrawerLayout.closeDrawers();
+
+        int id = menuItem.getItemId();
+        String title = menuItem.getTitle().toString();
+
+        if(id == R.id.history){
+            Toast.makeText(context, title + ": 계정 정보를 확인합니다.", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.logout){
+            Toast.makeText(context, title + " Success!", Toast.LENGTH_SHORT).show();
+            signOut();
+            startMyActivity(LoginActivity.class);
+        }
+        return true;
     }
 }
